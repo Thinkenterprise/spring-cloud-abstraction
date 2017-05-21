a1c88b5ce03c3dada6218cee88fa030589fa3572
schaefem <schaefem@MSGN04551.int.root.msg.ag>
2017-05-21 16:23:23 +0200

    Implementation of AWS Cloud Abstraction

---
+++ spring-abstract-fortune-service/src/main/java/com/thinkenterprise/ApplicationAWSConfiguration.java
@@ -0,0 +1,24 @@
+package com.thinkenterprise;
+
+import org.springframework.cloud.aws.context.config.annotation.EnableContextCredentials;
+import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData;
+import org.springframework.cloud.aws.context.config.annotation.EnableContextRegion;
+import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
+import org.springframework.cloud.aws.jdbc.config.annotation.RdsInstanceConfigurer;
+import org.springframework.cloud.aws.jdbc.datasource.DataSourceFactory;
+import org.springframework.cloud.aws.jdbc.datasource.TomcatJdbcDataSourceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+@Configuration
+@Profile("aws")
+@EnableContextCredentials(accessKey="AKIAI763FZQF5VVPHCNA",secretKey="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",instanceProfile=true)
+@EnableContextRegion(autoDetect=true)
+@EnableContextInstanceData
+@EnableRdsInstance(dbInstanceIdentifier = "fortuneDb",password = "fortuneDb", username="fortuneDb", databaseName="fortuneDb")
+public class ApplicationAWSConfiguration {
+
+  
+}

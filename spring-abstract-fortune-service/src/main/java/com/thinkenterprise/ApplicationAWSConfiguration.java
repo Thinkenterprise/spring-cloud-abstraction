a90fb93c3c6b9f824ce868d4c3122b5b8bf72b0c
schaefem <schaefem@MSGN04551.int.root.msg.ag>
2017-05-23 20:30:14 +0200

    Implementation of Fortune AWS

--- spring-abstract-fortune-service/src/main/java/com/thinkenterprise/ApplicationAWSConfiguration.java
+++ spring-abstract-fortune-service/src/main/java/com/thinkenterprise/ApplicationAWSConfiguration.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2017 Thinkenterprise
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * @author Michael Schaefer
+ */
+
+
 package com.thinkenterprise;
 
 import org.springframework.cloud.aws.context.config.annotation.EnableContextCredentials;
@@ -13,12 +33,11 @@ import org.springframework.context.annotation.Profile;
 
 
 @Configuration
-@Profile("aws")
-@EnableContextCredentials(accessKey="",secretKey="",instanceProfile=true)
+@EnableContextCredentials(accessKey="",secretKey="")
 @EnableContextRegion(autoDetect=true)
 @EnableContextInstanceData
 @EnableRdsInstance(dbInstanceIdentifier = "fortuneDb",password = "fortuneDb", username="fortuneDb", databaseName="fortuneDb")
 public class ApplicationAWSConfiguration {
-
-  
+	
+	
 }
